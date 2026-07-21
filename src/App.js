f1ff9614cd15e37f68b057ac2fd3ac8b2adee639
maxcipherx-hub <maxcipherx@gmail.com>
2026-07-22 02:10:52 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Discipline OS is Live! 🚀</h1>
+      <p>आपका ऐप सफलतापूर्वक डिप्लॉय हो गया है।</p>
+    </div>
+  );
+}
+
+export default App;
+
